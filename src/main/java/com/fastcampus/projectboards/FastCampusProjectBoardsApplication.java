e0bf371732f2633b31fcddbff73220d148465859
Cho Jung Woo <wjddnwh12@naver.com>
2022-08-11 18:50:28 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/fastcampus/projectboards/FastCampusProjectBoardsApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectBoardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCampusProjectBoardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/projectboards/FastCampusProjectBoardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
