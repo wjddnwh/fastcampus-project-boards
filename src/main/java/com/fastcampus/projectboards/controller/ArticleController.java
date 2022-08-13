8b22fc1acedcd638b6eac78effa005872d709196
Cho Jung Woo <wjddnwh12@naver.com>
2022-08-13 22:13:44 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세로 뽑는다.

---
+++ src/main/java/com/fastcampus/projectboards/controller/ArticleController.java
@@ -0,0 +1,9 @@
+package com.fastcampus.projectboards.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
